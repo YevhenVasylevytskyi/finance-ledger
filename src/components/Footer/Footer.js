e8ba5c0ded98da6c079dde34f0b5f12bf9c90b82
Yevhen Vasylevytskyi <yevhenvasylevytskyi@gmail.com>
2022-09-27 13:05:49 +0300

    add links to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,18 +5,34 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
-        <svg className={styles.socialIcon}>
-          <use href={`${icons}#facebook`}></use>
-        </svg>
-        <svg className={styles.socialIcon}>
-          <use href={`${icons}#twitter`}></use>
-        </svg>
-        <svg className={styles.socialIcon}>
-          <use href={`${icons}#youtube`}></use>
-        </svg>
-        <svg className={styles.socialIcon}>
-          <use href={`${icons}#linkedin`}></use>
-        </svg>
+        <a
+          href='https://www.facebook.com/evgeniy.vasilevitskiy'
+          className={styles.socialLink}>
+          <svg className={styles.socialIcon}>
+            <use href={`${icons}#facebook`}></use>
+          </svg>
+        </a>
+        <a
+          href='https://twitter.com/SpiritUa87'
+          className={styles.socialLink}>
+          <svg className={styles.socialIcon}>
+            <use href={`${icons}#twitter`}></use>
+          </svg>
+        </a>
+        <a
+          href='https://www.youtube.com/'
+          className={styles.socialLink}>
+          <svg className={styles.socialIcon}>
+            <use href={`${icons}#youtube`}></use>
+          </svg>
+        </a>
+        <a
+          href='https://www.linkedin.com/in/yevhen-vasylevytskyi-727373212/'
+          className={styles.socialLink}>
+          <svg className={styles.socialIcon}>
+            <use href={`${icons}#linkedin`}></use>
+          </svg>
+        </a>
         
       </div>
       <p className={styles.footerText}>
